3f82de0795797566ea94017dfd4201003ea656e8
ottav14 <dominicjottaviano@gmail.com>
2025-07-27 16:04:01 -0400

    Added curves

---
+++ public/draw.js
@@ -0,0 +1,14 @@
+
+export const fillCircle = (ctx, x, y, r, col) => {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2 * Math.PI); 
+	ctx.fillStyle = col;
+	ctx.fill();
+}
+
+export const strokeCircle = (ctx, x, y, r, col) => {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2 * Math.PI); 
+	ctx.strokeStyle = col;
+	ctx.stroke();
+}
--- public/main.js
+++ public/main.js
@@ -1,10 +1,11 @@
 import * as PARAMS from './params.js';
 import * as UTIL from './util.js';
+import * as DRAW from './draw.js';
 import Pendulum from './Pendulum.js';
 import DoublePendulum from './DoublePendulum.js';
 import initFractalDisplay from './FractalDisplay.js';
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('main-canvas');
 const ctx = canvas.getContext('2d'); 
 let dps = [];
 const timestep = 0.001;
@@ -17,7 +18,8 @@ const names = [
 	'mass1',
 	'mass2',
 	'stacked',
-	'angleDisplay'
+	'angleDisplay',
+	'curves'
 ];
 
 const state = {
@@ -31,10 +33,11 @@ const state = {
 	width: 10,
 	radius: 30,
 	stacked: false,
-	angleDisplay: false
+	angleDisplay: false,
+	curves: false
 }
 
-const clearCanvas = () => {
+const clearCanvas = (ctx) => {
 	ctx.fillStyle = `rgba(255, 255, 255, ${1-state.blur/100})`;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
@@ -69,6 +72,49 @@ const updateAngleDisplay = () => {
 	state.angleDisplay = document.getElementById('angleDisplay').checked;
 }
 
+const setPivotPoints = () => {
+	const y = 200;
+	for(let i=0; i<state.count; i++) {
+		const x = state.stacked ? 0.5*canvas.width : (i+1)*canvas.width/(state.count+1);
+		dps[i].p1.x = x;
+		dps[i].p1.y = y;
+	}
+}
+
+const toggleCurves = () => {
+	state.curves = document.getElementById('curves').checked;
+	const curvesCanvas = document.getElementById('curves-canvas');
+	const curvesCtx = curvesCanvas.getContext('2d');
+
+	if(state.curves) {
+		curvesCanvas.classList.remove('hidden');
+		canvas.width = 512;
+	}
+	else {
+		curvesCanvas.classList.add('hidden');
+		canvas.width = 1024;
+	}
+	setPivotPoints();
+	clearCanvas(curvesCtx);
+}
+
+const updateCurves = () => {
+	const curvesCanvas = document.getElementById('curves-canvas');
+	const curvesCtx = curvesCanvas.getContext('2d');
+
+	curvesCtx.fillStyle = 'rgba(255, 255, 255, 0.01)';
+	curvesCtx.fillRect(0, 0, curvesCanvas.width, curvesCanvas.height);
+
+	for(let i=0; i<dps.length; i++) {
+		const x = 1/2*curvesCanvas.width / Math.PI * UTIL.mod(dps[i].p1.t+0.5*Math.PI, 2*Math.PI);
+		const y = 1/2*curvesCanvas.height / Math.PI * UTIL.mod(dps[i].p2.t+0.5*Math.PI, 2*Math.PI);
+		const [r, g, b] = UTIL.hue(10*i%255);
+		DRAW.fillCircle(curvesCtx, x, y, 20, `rgba(${r}, ${g}, ${b}, 1)`);
+	}
+
+	
+}
+
 const initControls = () => {
 
 	for(const name of names) {
@@ -79,12 +125,18 @@ const initControls = () => {
 	const angleDisplayControl = document.getElementById('angleDisplay');
 	angleDisplayControl.removeEventListener('input', updateInitialConditions);
 	angleDisplayControl.addEventListener('input', updateAngleDisplay);
+
+	const curvesControl = document.getElementById('curves');
+	curvesControl.removeEventListener('input', updateInitialConditions);
+	curvesControl.addEventListener('input', toggleCurves);
 }
 initControls();
 
 const init = () => {
 
-	clearCanvas();
+	const curvesCtx = document.getElementById('curves-canvas').getContext('2d');
+	clearCanvas(ctx);
+	clearCanvas(curvesCtx);
 	dps = [];
 	PARAMS.setDPCount(0);
 
@@ -101,14 +153,19 @@ document.addEventListener('DOMContentLoaded', updateInitialConditions);
 
 function update() {
 
-	clearCanvas();
+	clearCanvas(ctx);
 	for(const dp of dps) {
 		dp.update();
 		dp.display(ctx, state.angleDisplay);
 	}
 	time += timestep;
 
+	updateCurves();
+
   requestAnimationFrame(update);
 }
 requestAnimationFrame(update);
+
+toggleCurves();
+updateCurves();
 
